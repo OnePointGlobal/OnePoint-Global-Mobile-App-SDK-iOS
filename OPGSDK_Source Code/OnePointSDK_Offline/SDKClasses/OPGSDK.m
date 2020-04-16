987eb61ff4b838222bf10d431468a1e818088496
Durga Achyuth <50234364+durgajadapolu@users.noreply.github.com>
2020-04-16 10:55:20 +0530

    Updated SDK for live environment for testing!

--- OPGSDK_Source Code/OnePointSDK_Offline/SDKClasses/OPGSDK.m
+++ OPGSDK_Source Code/OnePointSDK_Offline/SDKClasses/OPGSDK.m
@@ -60,9 +60,9 @@ static BOOL isResourceFound;
 +(void)initializeWithUserName:(NSString *)userName withSDKKey:(NSString *)key
 {
     NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
-    [defaults setObject:QCInterviewUrl forKey:@"OPGInterviewUrl"];
-    [defaults setObject:QCApiURL forKey:@"OPGApiUrl"];
-    [defaults setObject:QCDownloadMediaURL forKey:@"OPGDownloadMediaUrl"];
+    [defaults setObject:LiveInterviewUrl forKey:@"OPGInterviewUrl"];
+    [defaults setObject:LiveApiURL forKey:@"OPGApiUrl"];
+    [defaults setObject:LiveDownloadMediaURL forKey:@"OPGDownloadMediaUrl"];
 
     isResourceFound = [self isResourceBundleAvailable];
     if (!isResourceFound) {
